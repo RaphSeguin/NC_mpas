3f73e679f1c217af1e5249065ef036ae87868346
RaphSeguin <raphaelseguin@protonmail.com>
2023-11-28 10:07:51 +0100

    Update on make

--- make.R
+++ make.R
@@ -7,15 +7,13 @@
 
 #-----------------Loading packages-------------------
 
-pkgs <- c("tidyverse","here","lme4","broom","units","tidymodels","parallel","cowplot","janitor","sf","RColorBrewer","ggridges","plotly","heatmaply","parsedate","birk","ggthemes","MASS","automap","pbmcapply","ggnewscale","CausalImpact","sjmisc","ggspatial",
+pkgs <- c("tidyverse","here","lme4","broom","units","tidymodels","parallel","cowplot","janitor","sf","RColorBrewer","ggridges","plotly","heatmaply","parsedate","birk","ggthemes","MASS","automap","pbmcapply","ggnewscale","CausalImpact","sjmisc","ggspatial","stringr",
           "harrypotter","wesanderson","ranger","ggpubr","data.table","xml2","XML","rnaturalearth","ggExtra","raster","exactextractr","gstat","magrittr","scales","grid","gridExtra","XML","gfwr","fasterize")
 nip <- pkgs[!(pkgs %in% installed.packages())]
 nip <- lapply(nip, install.packages, dependencies = TRUE)
 ip   <- unlist(lapply(pkgs, require, character.only = TRUE, quietly = TRUE))
 
 sf_use_s2(FALSE)
-
-
 #-----------------Loading all data---------------------
 
 getwd()
@@ -26,14 +24,12 @@ setwd(path)
 files <- list.files(pattern="Rdata")
 data_list = lapply(files, load, .GlobalEnv)
 
-
 #-----------------Loading all functions---------------------
 
 path = (here::here("R"))
 setwd(path)
 files.source = list.files(here::here("R"))
 sapply(files.source, source)
-
 
 #-----------------Loading all outputs---------------------
 
@@ -52,14 +48,46 @@ parc_corail <- sf::st_read("maps/limite_parc_corail/limite_parc_corail.shp") %>%
 reserve_corail <- sf::st_read("maps/limite_reserve_corail/limite_reserve_corail.shp") %>% st_transform(crs = 4326)
 reserve_corail_detail <- sf::st_read("maps/reserve_detail/reserve_detail.shp") %>% st_transform(crs = 4326)
 
+angle2dec <- function(angle) {
+  angle <- as.character(angle)
+  x <- do.call(rbind, strsplit(angle, split=' '))
+  x <- apply(x, 1L, function(y) {
+    y <- as.numeric(y)
+    y[1] + y[2]/60 + y[3]/3600
+  })
+  return(x)
+}
+new_reserves_NC <- list.files(path = "maps/shapefiles_reserves_new/",
+                              pattern="*.csv",
+                              full.names = T) %>%
+  map_df(~read_csv(.))
+# new_reserves_NC <- read.csv("maps/shapefiles_reserves_new/banc_capel.csv")
+
+#Working on this DF
+# new_reserves_sf <- new_reserves_NC %>%
+#   #Cleaning DF 
+#   dplyr::mutate(Latitude = str_squish(gsub("[^0-9.-]", " ", new_reserves_NC$Latitude)),
+#          Longitude =str_squish(gsub("[^0-9.-]", " ", new_reserves_NC$Longitude))) %>%
+#   dplyr::mutate(Latitude = angle2dec(Latitude),
+#                 Longitude = angle2dec(Longitude)) %>%
+#   dplyr::mutate(Latitude = -Latitude,
+#                 Longitude = Longitude) %>%
+#   dplyr::rename(name = "Name") %>%
+#   st_as_sf(coords = c("Longitude","Latitude"),crs=4326) %>%
+#   st_transform(crs = st_crs(parc_corail)) %>%
+#   # Converting to polygon
+#   group_by(name) %>%
+#   summarise(geometry = st_combine(geometry)) %>%
+#   st_cast("POLYGON") %>%
+#   ungroup() %>%
+#   mutate(PA_category = ifelse(name == "Banc Capel","Réserve intégrale","Réserve naturelle"))
+# 
+# st_write(new_reserves_sf,dsn = "maps/new_reserves_sf.shp")
+new_reserves_sf <- st_read("maps/shapefiles_reserves_new/new_reserves_NC.shp")
+
 #Map of new caledonia
 #Download from https://maps.princeton.edu/catalog/stanford-qr598rg4243
 NC <- sf::st_read("maps/qr598rg4243.shp") %>% st_transform(crs = st_crs(parc_corail))
-
-#New reservess
-new_reserves_sf <- st_read("maps/new_reserves/new_reserves_sf.shp") %>%
-  st_transform(crs = st_crs(parc_corail)) %>%
-  mutate(PA_category = ifelse(name == "Banc Capel","Réserve intégrale","Réserve naturelle"))
 
 #NC EBSAs
 norfolk_ebsa <- st_read("maps/norfolk_ebsa.shp") %>% st_transform(crs = st_crs(parc_corail)) %>% st_intersection(parc_corail)  %>% mutate(Category = "EBSA")
@@ -71,7 +99,6 @@ NC_imma <- st_read("maps/iucn-imma/iucn-imma.shp") %>% st_transform(crs = st_crs
 
 #IBAs
 NC_IBA <- st_read("maps/New_Calendonia_IBA/New_Calendonia_IBA.shp") %>% st_transform(crs = st_crs(parc_corail)) %>% st_intersection(parc_corail) %>% mutate(Category = "IBA")
-
 NC_ebsa <- bind_rows(norfolk_ebsa,northern_low_ridge_ebsa,hebrides_ebsa,NC_imma,NC_IBA)
 
 #Seamounts
@@ -84,8 +111,8 @@ NC_seamounts <- bind_rows(seamounts_NC, knolls_NC)
 #All reserves
 all_reserves = new_reserves_sf %>%
   bind_rows(reserve_corail) %>%
-  mutate(name = ifelse(is.na(name),nom,name)) %>%
-  mutate(type = ifelse(is.na(id),"Réserve existante","Nouvelle réserve"))
+  mutate(name = ifelse(is.na(nom),name,nom)) %>%
+  mutate(type = ifelse(is.na(nom),"Nouvelle réserve","Réserve existante"))
 
 #--------------NEW CALEDONIA----------------------------
 setwd(here())
@@ -110,34 +137,40 @@ AIS_protected_area <- associate_detections_AIS(fishing_effort)
 #Calculations
 AIS_fishing_effort <- calculate_fishing(AIS_protected_area)
 
-#Creating smaller maps
-
-
-fishing_effort_plot <- ggplot(data = AIS_fishing_effort,aes(time_range,fishing_effort,color = reserve)) +
-  geom_line(size = 1) +
-  theme_minimal() +
-  scale_color_hp_d(option = "Ravenclaw",labels = c("Pas de réserve","Nouvelles réserves")) +
-  labs(x = " ",
-       y= "Effort de pêche par km^2",
-       color = "Protection")
+#Fishing effort plot
+(fishing_effort_plot <- ggplot(data = AIS_fishing_effort,aes(time_range,fishing_effort,color = reserve)) +
+    geom_line(linewidth = 1) +
+    theme_minimal() +
+    scale_color_hp_d(option = "Ravenclaw",labels = c("Hors réserves","Dans les futures réserves"),name = "Effort de pêche/km^2 entre 2021 et 2023") +
+    labs(x = " ",
+         y= "Effort de pêche par km^2",
+         color = "Protection") +
+    geom_vline(xintercept = as.numeric(as.Date("2023-07-27"))) +
+    scale_x_date(date_breaks = "1 month",limits = as.Date(c('2021-01-01','2023-10-31'))) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+    theme(legend.position = "bottom"))
 
 ggsave(fishing_effort_plot, file = "figures/fishing_effort_plot.pdf", width = 297, height = 210, units = "mm", dpi = 600)
 ggsave(fishing_effort_plot, file = "figures/fishing_effort_plot.jpg", width = 297, height = 210, units = "mm", dpi = 600)
 
 #Maps
-
-reserve_legend = c("Banc Capel"="#B45801",
-                   "Ride Entrecasteaux" = "#D7AD82",
-                   "Nord Walpole" = "#99C2D9",
-                   "Nouvelle Hebrides"="#3FA6D9",
-                   "Norfolk"="#1081B3",
-                   "Fairway"="#046698")
+reserve_legend = c("Banc Capel"="#3B9AB2",
+                   "Ride d?Entrecasteaux" = "#78B7C5",
+                   "Ile de Walpole" = "#EBCC2B",
+                   "Nouvelles Hebrides"="#F21A01",
+                   "Ride de Norfolk"="#E1AF02",
+                   "Sud Bassin de Fairway"="#ECCBAE")
 
 map <- ggplot() +
   geom_sf(data = NC, fill = "black")+
   geom_sf(data = parc_corail, fill = "lightblue")+
   geom_sf(data = new_reserves_sf, aes(fill = name),alpha = 0.8)+
-  scale_fill_manual(values = reserve_legend,name = "Réserve") +
+  scale_fill_manual(values = reserve_legend,name = "Réserve",
+                    labels = c("Banc Capel"="Banc Capel",
+                               "Ride d?Entrecasteaux" = "Ride d'Entrecasteaux",
+                               "Ile de Walpole" = "La Monique - Ile de Walpole",
+                               "Nouvelles Hebrides" = "Fosse des Nouvelles-Hébrides",
+                               "Sud Bassin de Fairway" = "Sud Bassin de Fairway"))+
   geom_sf(data = all_reserves, aes(color = type),pch = 21,linewidth = 0.8,alpha = 0) +
   scale_color_hp_d(option="Gryffindor",name = "Type") +
   geom_sf(data = fishing_effort, shape = ".") +
@@ -182,8 +215,15 @@ heatmap <- ggplot() +
   new_scale_fill() +
   geom_sf(data = NC, fill = "black") +
   geom_sf(data = parc_corail, fill = "lightblue", alpha = 0, pch = 21, color = "black") +
-  geom_sf(data = all_reserves %>% filter(type != "Parc naturel marin"), aes(fill = type),alpha = 0.5) +
-  scale_fill_hp_d(option = "Ravenclaw",name = "Type") + 
+  geom_sf(data = new_reserves_sf, aes(fill = name),alpha = 0.8)+
+  scale_fill_manual(values = reserve_legend,name = "Réserve",
+                    labels = c("Banc Capel"="Banc Capel",
+                               "Ride d?Entrecasteaux" = "Ride d'Entrecasteaux",
+                               "Ile de Walpole" = "La Monique - Ile de Walpole",
+                               "Nouvelles Hebrides" = "Fosse des Nouvelles-Hébrides",
+                               "Sud Bassin de Fairway" = "Sud Bassin de Fairway"))+
+  geom_sf(data = all_reserves, aes(color = type),pch = 21,linewidth = 0.4,alpha = 0) +
+  scale_color_hp_d(option = "Ravenclaw",name = "Type") +
   theme_map() +
   theme(legend.position = "bottom")+
   ggspatial::annotation_scale(location = "tr") +
@@ -201,17 +241,17 @@ ggsave(heatmap, file = "figures/heatmap.jpg", width = 297, height = 210, units =
 
 #Zone sensibles
 (area_sensible <- ggplot() + 
-  geom_sf(data = NC, fill = "black") +
-  geom_sf(data = parc_corail, pch = 22, colour = "black",alpha = 0) +
-  geom_sf(data = NC_ebsa, aes(fill = Category),alpha = 0.6) +
-  scale_fill_hp_d(option = "Gryffindor",name = "Zone Sensible", labels = c(EBSA = "Ecologically or Biologically Significant Marine Areas",
-    IMMA = "Important Marine Mammal Area",IBA = "Important Bird Areas")) +
-  new_scale_fill() +
-  geom_sf(data = all_reserves, aes(color = type,fill=type),pch = 21,linewidth = 1,alpha = 0.2) +
-  scale_color_hp_d(option="Ravenclaw",name = "Type") +
-  scale_fill_hp_d(option="Ravenclaw",name = "Type") +
-  theme_map() +
-  theme(legend.position = "bottom")+
+    geom_sf(data = NC, fill = "black") +
+    geom_sf(data = parc_corail, pch = 22, colour = "black",alpha = 0) +
+    geom_sf(data = NC_ebsa, aes(fill = Category),alpha = 0.6) +
+    scale_fill_hp_d(option = "Gryffindor",name = "Zone Sensible", labels = c(EBSA = "Ecologically or Biologically Significant Marine Areas",
+                                                                             IMMA = "Important Marine Mammal Area",IBA = "Important Bird Areas")) +
+    new_scale_fill() +
+    geom_sf(data = all_reserves, aes(color = type,fill=type),pch = 21,linewidth = 1,alpha = 0.2) +
+    scale_color_hp_d(option="Ravenclaw",name = "Type") +
+    scale_fill_hp_d(option="Ravenclaw",name = "Type") +
+    theme_map() +
+    theme(legend.position = "bottom")+
     ggspatial::annotation_scale(location = "tr") +
     ggspatial::annotation_north_arrow(
       location = "tr", which_north = "true",
@@ -221,7 +261,7 @@ ggsave(heatmap, file = "figures/heatmap.jpg", width = 297, height = 210, units =
         line_col = "grey20",
       )
     ))
-  
+
 ggsave(area_sensible, file = "figures/area_sensible.pdf",width = 297, height = 210, units = "mm",dpi=600)
 ggsave(area_sensible, file = "figures/area_sensible.jpg",width = 297, height = 210, units = "mm",dpi=600)
 
